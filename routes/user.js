9f06fea18f771664a6357735e3ae9171cbde1fc6
Soen Razatovo <soen.razatovo@gmail.com>
2026-03-26 12:02:20 +0100

    :art::heavy_plus_sign: Added package.json and JWT

--- routes/user.js
+++ routes/user.js
@@ -2,34 +2,55 @@ const express = require("express")
 const router = express.Router()
 
 const db = require("../config/db.js")
+const util = require('util');
+const queryAsync = util.promisify(db.query).bind(db);
+
 const bcrypt = require('bcrypt');
 
-router.post("/login", (req, res) => {
-    console.log(req.body)
-    let {email, password} = req.body
+const jwt = require("jsonwebtoken")
+require("dotenv").config()
 
-    res.json({content : "Login received", email}).status(200)
+
+router.post("/login", async (req, res) => {
+    try {
+        let {email, password} = req.body
+        const users = await queryAsync("SELECT * FROM Users WHERE email = ?", [email]) 
+
+        if (users.length == 1) {
+            if (await bcrypt.compare(password, users[0].password)){
+                
+                res.json("Connexion Valide").status(200)     
+
+                let token = jwt.sign({id : users[0].id, admin: true}, process.env.JWT_SECRET_KEY)
+                res.cookie("jwt", token, {expires: new Date(Date.now() + 86400 * 1000), httpOnly: true, secure: true})
+
+            } else {    
+                res.json("Mot de passe Invalide").status(401)
+            }
+        } else {
+            res.json("Email Invalide").status(401)
+        }
+    } catch (err) {
+        res.send(err).status(500)
+    }
+
 })
 
-router.post("/signup", (req, res) => {
-    console.log(req.body)
-    let {email, pseudo, password} = req.body
+router.post("/signup", async (req, res) => {
+    try {
+        let {email, pseudo, password} = req.body
 
-    bcrypt.hash(password, 10, (error, hash) => {
-        if (error) {
-            res.json({status: 500, content: error}).status(500)
+        const users = await queryAsync("SELECT * FROM Users WHERE email = ?", [email])
+        if (users.length == 0) {
+            const hash = await bcrypt.hash(password, 10)
+            const db_res = await queryAsync("INSERT INTO Users(name,email,password) VALUES (?,?,?)", [pseudo, email, hash])
+            res.status(200).json(db_res)
         } else {
-            const sql = "INSERT INTO Users(name,email,password) VALUES (?,?,?)"
-            db.query(sql, [pseudo, email, hash], (error, results) => {
-                if (error) {
-                    res.json({status: 500, content: error}).status(500)
-                } else {
-                    res.json({status: 200, content : "Signup Sucessful", email}).status(200)
-                }
-            })
+            res.json("Compte possède déjà cette email").status(401)
         }
-    })
-
+    } catch (err) {
+        res.status(500).json(err)
+    }
 })
 
 module.exports = router
